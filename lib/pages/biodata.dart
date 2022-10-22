8ffafd7c621fd49175d8fe688346c90eeccd15a5
royhandf <royhandaffa14@gmail.com>
2022-10-22 21:32:23 +0700

    mengubah button pada login, bentuk list members, membuat ui untuk biodata

--- lib/pages/biodata.dart
+++ lib/pages/biodata.dart
@@ -1,35 +1,49 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import '../utils/data.dart';
 
 class Biodata extends StatelessWidget {
   final int index;
 
-  const Biodata(this.index);
+  const Biodata(this.index, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: double.infinity,
-          width: double.infinity,
-          color: Colors.blue[200],
-          child: ListView(
-            children: [
-              Center(
-                child: Text("${members[index]['name']}"),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.asset(
+                "assets/${members[index]['profile']}",
+                height: 150,
+                width: 150,
+                fit: BoxFit.cover,
               ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Text(
-                  "${members[index]['description']}",
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(fontSize: 15),
-                ),
-              )
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Text(
+                "${members[index]['name']}",
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Text(
+                "${members[index]['biodata']}",
+                textAlign: TextAlign.justify,
+                style: const TextStyle(fontSize: 15),
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -3,28 +3,36 @@ List members = [
     'username': 'huda',
     'password': '123',
     'name': 'Muhammad Khoirul Huda',
+    'nim': '200605110085',
     'profile': 'huda.jpg',
-    'description': 'Huda adalah seorang mahasiswa yang sedang belajar flutter'
+    'biodata':
+        'Saya adalah seorang mahasiswa di Universitas Islam Negeri Maulana Malik Ibrahim Malang',
   },
   {
     'username': 'royhan',
     'password': '123',
     'name': 'M. Royhan Daffa',
+    'nim': '200605110104',
     'profile': 'royhan.jpg',
-    'description': 'Royhan adalah seorang mahasiswa yang sedang belajar flutter'
+    'biodata':
+        'Saya adalah seorang mahasiswa di Universitas Islam Negeri Maulana Malik Ibrahim Malang',
   },
   {
     'username': 'dzikri',
     'password': '123',
     'name': 'Muhammad Dzikri Zulkifly Riskha',
+    'nim': '200605110053',
     'profile': 'dzikri.jpg',
-    'description': 'Dzikri adalah seorang mahasiswa yang sedang belajar flutter'
+    'biodata':
+        'Saya adalah seorang mahasiswa di Universitas Islam Negeri Maulana Malik Ibrahim Malang',
   },
   {
     'username': 'yoga',
     'password': '123',
     'name': 'Yoga Pratama Kusendi',
+    'nim': '200605110084',
     'profile': 'yoga.jpg',
-    'description': 'Yoga adalah seorang mahasiswa yang sedang belajar flutter'
+    'biodata':
+        'Saya adalah seorang mahasiswa di Universitas Islam Negeri Maulana Malik Ibrahim Malang',
   }
 ];
